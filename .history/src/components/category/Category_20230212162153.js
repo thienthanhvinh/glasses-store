c5b534624173cd46e1e54768cb39f0a90af63d2f
Thanh Vinh <nguyenthienthanhvinh@gmail.com>
2023-02-15 10:41:09 +0700

    glasses store

--- .history/src/components/category/Category_20230212162153.js
+++ .history/src/components/category/Category_20230212162153.js
@@ -5,14 +5,14 @@ const Category = () => {
     return (
         <div className='flex mb-[112px] mx-auto max-w-[1400px] w-full gap-x-2'>
             <div className='h-[596px] relative'>
-                <img src="category1.png" alt="" className='w-full h-full object-cover' />
+                <img src="/category1.png" alt="" className='w-full h-full object-cover' />
                 <div className='absolute bottom-24 left-14'>
                     <p className='font-extrabold text-[40px] leading-[98px] text-white'>Optical</p>
                     <Button className='bg-secondary text-primary rounded-[4px] px-6 py-2 font-bold text-lg leading-[30px]'>Buy</Button>
                 </div>
             </div>
             <div className='h-[596px] relative'>
-                <img src="category2.png" alt="" className='w-full h-full object-cover' />
+                <img src="/category2.png" alt="" className='w-full h-full object-cover' />
                 <div className='absolute bottom-24 left-14'>
                     <p className='font-extrabold text-[40px] leading-[98px] text-white'>Sunwear</p>
                     <Button className='bg-secondary text-primary rounded-[4px] px-6 py-2 font-bold text-lg leading-[30px]'>Buy</Button>
--- .history/src/components/category/Category_20230215103826.js
+++ .history/src/components/category/Category_20230215103826.js
@@ -12,7 +12,7 @@ const Category = () => {
                 </div>
             </div>
             <div className='h-[596px] relative'>
-                <img src="category2.png" alt="" className='w-full h-full object-cover' />
+                <img src="/category2.png" alt="" className='w-full h-full object-cover' />
                 <div className='absolute bottom-24 left-14'>
                     <p className='font-extrabold text-[40px] leading-[98px] text-white'>Sunwear</p>
                     <Button className='bg-secondary text-primary rounded-[4px] px-6 py-2 font-bold text-lg leading-[30px]'>Buy</Button>
